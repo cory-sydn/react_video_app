59cb68eca1ad70bfe24ef62d7f7f883c5a2edec9
Koray Soydan <81196699+cory-sydn@users.noreply.github.com>
2022-11-26 02:14:58 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -44,10 +44,10 @@ app.use("/api/videos", videoRoutes)
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.use((err, req, res, next) => {

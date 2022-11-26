2c56d83c0c9822641f40b4e2d3bb14414e1c14cb
Koray Soydan <81196699+cory-sydn@users.noreply.github.com>
2022-11-26 10:05:05 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,13 +11,12 @@ import { fileURLToPath } from 'url';
 const app = express();
 
 dotenv.config();
-const ORIGIN = process.env.ORIGIN
 
 app.use(cookieParser())
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', ORIGIN);
+  res.header('Access-Control-Allow-Origin', `https://${process.env.VERCEL_URL}`);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 	res.header("Access-Control-Expose-Headers", "Set-Cookie",)
